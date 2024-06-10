b7cdcc1de3d2cf6b548732f3cd6eb20e2fa57cae
Aryan_pidiha <aryan.pidiha@siya.tech>
2024-06-10 12:59:13 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,6 @@ const navItemsData: Array<any> = [
 ];
 
 export default function Home() {
-  console.log("console");
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
